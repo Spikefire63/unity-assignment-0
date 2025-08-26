4ab783c12deff90aa62b08bdcf5e41e8158a0115
Spikefire63 <klitschmark@gmail.com>
2025-08-25 23:56:05 -0400

    lights
    world terrain would not accept the grass when going to push the previous push so i had to remove it.

--- unity assignment 0/Assets/scripts/Lights.cs
+++ unity assignment 0/Assets/scripts/Lights.cs
@@ -7,11 +7,14 @@ public class Lights : MonoBehaviour
     void Start()
     {
         Renderer renderer = GetComponent<Renderer>();
-        material = renderer.materials[0];
+        material = renderer.materials[1];
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        
+         Color currentEmission = material.GetColor("_EmissionColor");
+         material.SetColor("_EmissionColor", currentEmission * 0f);
         
     }
 }
